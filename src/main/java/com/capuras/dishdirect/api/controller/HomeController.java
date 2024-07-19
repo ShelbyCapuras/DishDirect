9d110a53ae9ced5ade746fa29eb36a51c4596b93
ShelbyCapuras <shelbycapuras22@gmail.com>
2024-07-19 11:59:49 +0800

    Update HomeController.java

--- src/main/java/com/capuras/dishdirect/api/controller/HomeController.java
+++ src/main/java/com/capuras/dishdirect/api/controller/HomeController.java
@@ -7,6 +7,7 @@ import springfox.documentation.annotations.ApiIgnore;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@CrossOrigin(origins="*")
 @Controller
 public class HomeController {
 
